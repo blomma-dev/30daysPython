e738b186ff8b593f2e25dc21f11eb7497319428f
Joel B <blomsterjoel@gmail.com>
2023-05-15 01:26:35 +0300

    removing imports not needed and add more comments

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -1,10 +1,10 @@
-#importing sys, platform and OS module
+# importing platform module to check the version of Python
 
-import sys
 import platform
-import os
 
+# printing the version of Python
 print("Your version of Python is: " + platform.python_version())
 
+# printing and explaining the use of the type() function
 print("You can check different types of variables with the type() function")
 
