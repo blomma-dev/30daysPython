3cd5871a250c238007a8149fdcdd2aeecbdc59a6
blomma-dev <blomsterjoel@gmail.com>
2023-05-15 01:09:50 +0300

    create helloworld.py and print python host version

---
+++ day_1/helloworld.py
@@ -0,0 +1,10 @@
+#importing sys, platform and OS module
+
+import sys
+import platform
+import os
+
+print("Your version of Python is: " + platform.python_version())
+
+print("You can check different types of variables with the type() function")
+
