39c0a02d4aa42083a8a3193695435f6cb2e58fd9
Joel B <blomsterjoel@gmail.com>
2023-05-15 01:46:15 +0300

    created variables.py and added a few variables

---
+++ day_2/variables.py
@@ -0,0 +1,13 @@
+# "day" 2: 30 days of python programming
+
+first_name = "Joel"
+last_name = "Blomster"
+full_name = first_name + " " + last_name
+country = "Finland"
+city = "Turku"
+age = 25
+birth_year = 1998
+is_married = False
+in_relationship = True
+theme_preferred = "Dark"
+
