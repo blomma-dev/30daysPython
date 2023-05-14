e58d076e767ce8b053b15b0631e10b9eb728b3f6
Joel B <blomsterjoel@gmail.com>
2023-05-15 02:39:04 +0300

    added more variables and played with operators

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,5 +1,7 @@
 # "day" 2: 30 days of python programming
 
+# declaring a few variables
+
 first_name = "Joel"
 last_name = "Blomster"
 full_name = first_name + " " + last_name
@@ -11,6 +13,8 @@ is_married = False
 in_relationship = True
 theme_preferred = "Dark"
 
+# printing the type of the variables above
+
 print(type(first_name))
 print(type(last_name))
 print(type(full_name))
@@ -21,3 +25,20 @@ print(type(birth_year))
 print(type(is_married))
 print(type(in_relationship))
 print(type(theme_preferred))
+
+# printing the length of my first and last name
+
+print(len(first_name))
+print(len(last_name))
+
+# playing a little with operators
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
