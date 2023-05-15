996c8aec21b4ca3576a571f7e4e23c6100564c3f
Joel B <blomsterjoel@gmail.com>
2023-05-15 15:47:10 +0300

    added day_3 and created little math problems

---
+++ day_3/day3.py
@@ -0,0 +1,11 @@
+# exercises for "day3" of the 30 days of python challenge
+
+my_age = 25
+my_height = 1.75
+difficult_number = 7j
+
+base = input("Enter the base of the triangle: ")
+height = input("Enter the height of the triangle: ")
+area = 0.5 * int(base) * int(height)
+print("The area of the triangle is: " + str(area))
+
